7dc17661452e8e23db77a2ae35e34f9e834a4f59
Mirko Rinke <mirkorinke@htp.com>
2025-02-24 22:14:39 +0100

    Add role column to users table and update API routes for registration

--- routes/api.php
+++ routes/api.php
@@ -14,14 +14,14 @@ Route::get('/user', function (Request $request) {
 
 
 
-// Public routes that do not require authentication
+// Public routes that do not require authentication route to login and get a token
 Route::post('/login', [AuthController::class, 'login']);
 
-
-
+// Public routes that do not require authentication route to register a new user
+Route::post('/register', [UserApiController::class, 'store']);
 
 // Protected routes that require authentication using Sanctum middleware
 Route::middleware('auth:sanctum')->group(function () {
     // User resource route with the controller specified in the second argument
-    Route::apiResource('users', UserApiController::class); 
+    Route::apiResource('users', UserApiController::class)->except(['store']);
 });
