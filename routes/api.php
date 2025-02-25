7bb596f58de2a119ed00d939f7e916358f890331
Mirko Rinke <mirkorinke@htp.com>
2025-02-25 01:16:23 +0100

    Add CheckRole middleware and update user routes for admin role management

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Api\AuthController; // Import the controller to use it 
 
 use App\Http\Controllers\Api\UserApiController; // Import the controller to use it in the routes file
 
+use App\Http\Middleware\CheckRole; // Import the middleware to use it in the routes file
+
 Route::get('/user', function (Request $request) { 
     return $request->user();
 })->middleware('auth:sanctum');
@@ -23,5 +25,11 @@ Route::post('/register', [UserApiController::class, 'store']);
 // Protected routes that require authentication using Sanctum middleware
 Route::middleware('auth:sanctum')->group(function () {
     // User resource route with the controller specified in the second argument
-    Route::apiResource('users', UserApiController::class)->except(['store']);
+    Route::apiResource('users', UserApiController::class)->except(['store', 'destroy']);
+});
+
+
+// Protected routes that require authentication and the role of the user is admin
+Route::middleware(['auth:sanctum', CheckRole::class . ':admin'])->group(function () {
+    Route::delete('/users/{user}', [UserApiController::class, 'destroy']);
 });
