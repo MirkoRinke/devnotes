0cef0d6f501558862255e3d16dc5853ba38fece6
Mirko Rinke <mirkorinke@htp.com>
2025-02-27 18:23:41 +0100

    Update posts table migration to set user_id to null on delete

--- database/migrations/2025_02_26_191557_create_posts_table.php
+++ database/migrations/2025_02_26_191557_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void  {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained('users', 'id');
+            $table->foreignId('user_id')->nullable()->constrained('users', 'id')->onDelete('set null');
             $table->string('title');
             $table->text('code');
             $table->text('description');
