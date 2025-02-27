4e235c915930d9d55f0eafa6bf8da21c96ea8d37
Mirko Rinke <mirkorinke@htp.com>
2025-02-27 22:48:27 +0100

    Set token expiration to 7 days in AuthController login method

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,8 +8,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
-class AuthController extends Controller
-{
+use Carbon\Carbon;
+
+class AuthController extends Controller {
     public function login(Request $request)
     {
         $request->validate([
@@ -17,15 +18,23 @@ class AuthController extends Controller
             'password' => 'required',
             'device_name' => 'required',
         ]);
-
+    
         $user = User::where('email', $request->email)->first();
-
+    
         if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
-
-        return $user->createToken($request->device_name)->plainTextToken;
+    
+        $token = $user->createToken($request->device_name);
+        $token->accessToken->expires_at = Carbon::now()->addDays(7);
+        $token->accessToken->save();
+    
+        return $token->plainTextToken;
     }
 }
+
+
+
+
