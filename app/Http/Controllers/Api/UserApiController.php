c140b4e72429229768bb02f5289ddf2d20ff8c98
Mirko Rinke <mirkorinke@htp.com>
2025-03-02 18:47:36 +0100

    Integrate ApiFiltering trait into UserApiController to enable query filtering based on request parameters

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\JsonResponse; // Import the JsonResponse class to use it in 
 
 use App\Traits\ApiResponses; // Import the ApiResponses trait to use it in the controller example $this->successResponse($users, 'Users retrieved successfully', 200);
 use App\Traits\ApiSorting; // Import the ApiSorting trait to use it in the controller example $this->sort($request, $query, [ 'id','name', 'email']);
+use App\Traits\ApiFiltering; // Import the ApiFiltering trait to use it in the controller example $this->filter($request, $query, [ 'name', 'email']);
 
 use Exception; // Import the Exception class
 use Illuminate\Validation\ValidationException; // Import the ValidationException class
@@ -19,21 +20,36 @@ class UserApiController extends Controller {
 
     use ApiResponses; // Use the ApiResponses trait in the controller
     use ApiSorting; // Use the ApiSorting trait in the controller
+    use ApiFiltering; // Use the ApiFiltering trait in the controller
 
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request): JsonResponse {
         try {
-            $query = User::query();
-            $query = $this->sort($request, $query, [ 'id','name', 'email']); // AllowedColumns is an array of columns that can be sorted
+            $query = User::query(); 
 
+            $query = $this->sort($request, $query, [ 'id','name', 'email']); // AllowedColumns is an array of columns that can be sorted
+            
             // Check return value of the sort method and return the response if status code is 400
             if ($query instanceof JsonResponse && $query->getStatusCode() === 400) {
                 return $query;
-            }            
+            }
+            
+            // Filter the query results based on the request
+            $query = $this->filter($request, $query, [ 'name', 'email']); // AllowedColumns is an array of columns that can be filtered
+
+            // Check return value of the filter method and return the response if status code is 400
+            if ($query instanceof JsonResponse && $query->getStatusCode() === 400) {
+                return $query;
+            }
 
             $query = $query->get(); // Get all the users
+
+            // Check if the query is empty and return a response message
+            if ($query->isEmpty()) {
+                return $this->successResponse($query, 'No users found with the given filters', 200);
+            }
 
             return $this->successResponse($query, 'Users retrieved successfully', 200);
         } catch (Exception $e) {
