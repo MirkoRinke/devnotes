46ef921d9c69f7980652728e0e9edeaf9666412d
Mirko Rinke <mirkorinke@htp.com>
2025-03-02 20:57:53 +0100

    Add custom JSON response for RouteNotFoundException in app configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+use Illuminate\Http\Request;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -15,5 +18,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->render(function (RouteNotFoundException $e, Request $request) {
+            if (!$request->expectsJson()) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Unauthorized',
+                    'code' => 401,
+                    'data' => null
+                ], 401);
+            }
+        });
+    })
+    ->create();
