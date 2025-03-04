b90271383bc076bb808f53937f8904c51e083604
Mirko Rinke <mirkorinke@htp.com>
2025-03-04 19:26:04 +0100

    Refactor filter method in ApiFiltering trait for improved query handling

--- app/Traits/ApiFiltering.php
+++ app/Traits/ApiFiltering.php
@@ -23,31 +23,26 @@ trait ApiFiltering {
      * @return JsonResponse|Builder
      */
     public function filter(Request $request,Builder $query, $allowedFilterColumns = []): JsonResponse|Builder{
-
         // Get the filter array from the request
-        $filterArray = $request->query('filter', []);
-   
-        // For each filter column, add a where clause to the query
-        foreach ($filterArray as $key => $values) {
+        $filterArray = $request->query('filter');
 
-            // Check if the filter column is allowed
-            if (!in_array($key, $allowedFilterColumns)) {
-                return $this->errorResponse('Invalid filter column: ' . $key, ['filter' => 'INVALID_FILTER_COLUMN'], 400);
+        if ($filterArray) {         
+            // For each filter column, add a where clause to the query
+            foreach ($filterArray as $key => $values) {
+                // Check if the filter column is allowed
+                if (!in_array($key, $allowedFilterColumns)) {
+                    return $this->errorResponse('Invalid filter column: ' . $key, ['filter' => 'INVALID_FILTER_COLUMN'], 400);
+                }
+                // If the value is not an array, convert it to an array
+                if (!is_array($values)) {
+                    $values = explode(',', $values); 
+                }    
+                // For each value, add a where clause to the query to filter the results
+                foreach ($values as $value) {
+                    $query->orWhere($key, 'LIKE', '%' . $value . '%');
+                }
             }
-
-            // If the value is not an array, convert it to an array
-            if (!is_array($values)) {
-                $values = explode(',', $values); 
-            }
-   
-            // For each value, add a where clause to the query to filter the results
-            foreach ($values as $value) {
-                $query->orWhere($key, 'LIKE', '%' . $value . '%');
-            }
-
         }
-
         return $query;
-    }
-    
+    }    
 }
