61ea337bd973ade3e435c48131d40a79e064c332
Mirko Rinke <mirkorinke@htp.com>
2025-03-04 16:52:32 +0100

    Add ApiPagination trait to UserApiController for pagination support

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -12,7 +12,7 @@ use App\Traits\ApiResponses; // Import the ApiResponses trait to use it in the c
 use App\Traits\ApiSorting; // Import the ApiSorting trait to use it in the controller example $this->sort($request, $query, [ 'id','name', 'email']);
 use App\Traits\ApiFiltering; // Import the ApiFiltering trait to use it in the controller example $this->filter($request, $query, [ 'name', 'email']);
 use App\Traits\SelectableAttributes; // Import the SelectableAttributes trait to use it in the controller example $this->selectAttributes($request, $query, [ 'id','name', 'email']);
-
+use App\Traits\ApiPagination; // Import the ApiPagination trait to use it in the controller example $this->getPerPage($request, $query, 10);
 
 use Exception; // Import the Exception class
 use Illuminate\Validation\ValidationException; // Import the ValidationException class
@@ -20,8 +20,8 @@ use Illuminate\Database\Eloquent\ModelNotFoundException; // Import the ModelNotF
 
 class UserApiController extends Controller {
 
-    // Use the ApiResponses, ApiSorting, ApiFiltering and SelectableAttributes traits in the controller
-    use ApiResponses, ApiSorting, ApiFiltering , SelectableAttributes;
+    // Use the ApiResponses, ApiSorting, ApiFiltering , SelectableAttributes and ApiPagination traits in the controller
+    use ApiResponses, ApiSorting, ApiFiltering , SelectableAttributes , ApiPagination;
 
     /**
      * Display a listing of the resource.
@@ -53,12 +53,14 @@ class UserApiController extends Controller {
             if ($query instanceof JsonResponse && $query->getStatusCode() === 400) {
                 return $query;
             }
-
-            // Set the number of items to be returned based on the request per_page array or default to 10
-            $perPage = $request->input('per_page') ?? 10;
-
-            // Get the query results
-            $query = $query->paginate($perPage);
+ 
+            // Paginate the query results based on the request per_page parameter
+            $query = $this->getPerPage($request, $query, 10);
+ 
+            // Check return value of the getPerPage method and return the response if status code is 400
+            if ($query instanceof JsonResponse && $query->getStatusCode() === 400) {
+                return $query;
+            }
 
             // Check if the query is empty and return a response message
             if ($query->isEmpty()) {
