1276109247cc666842ed32fbddd5af93fb9b7e47
Mirko Rinke <mirkorinke@htp.com>
2025-03-04 19:25:56 +0100

    Refactor getPerPage method in ApiPagination trait for improved readability

--- app/Traits/ApiPagination.php
+++ app/Traits/ApiPagination.php
@@ -25,21 +25,18 @@ trait ApiPagination {
     public function getPerPage(Request $request, Builder $query, int $default = 10): JsonResponse|Collection|LengthAwarePaginator  {
         // Get the page and per_page parameters from the request
         $page = $request->get('page');
-
+        
         if ($page) {      
             // Get the per_page parameter from the request
-            $perPage = $request->get('per_page', $default);
-            
+            $perPage = $request->get('per_page', $default);            
             // Validate the page and per_page parameters
             if (!is_numeric($page) || $page < 1) {
                 return $this->errorResponse('The page parameter must be a number greater than 0', ['page' => 'INVALID_PAGE_NUMBER'], 400);
             }
-
             // Validate the per_page parameter
             if (!is_numeric($perPage) || $perPage < 1) {
                 return $this->errorResponse('The per_page parameter must be a number greater than 0', ['per_page' => 'INVALID_PER_PAGE_NUMBER'], 400);
             }
-
             // Paginate the query results and return the results to the client 
             return $query->paginate((int) $perPage);
         }
