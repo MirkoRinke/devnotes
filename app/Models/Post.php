34d6a77644013de32a803a2186c38644a9d1f4c3
Mirko Rinke <mirkorinke@htp.com>
2025-03-06 01:51:53 +0100

    Add favorites relationship to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\User;
+use App\Models\UserFavorite;
+
 
 class Post extends Model {
+    
     use HasFactory;
 
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
     protected $fillable = [
         'user_id',
         'title',
@@ -24,8 +32,20 @@ class Post extends Model {
 
     /**
      * Get the user that owns the post.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * Get the favorites for the post.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function favorites() {
+        return $this->hasMany(UserFavorite::class);
+    }
+
 }
