ec97cbb1a80a7cf8c9462e14549332b3cca8cbec
Mirko Rinke <mirkorinke@htp.com>
2025-03-06 01:51:18 +0100

    Add UserFavorite model with relationships to User and Post

---
+++ app/Models/UserFavorite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserFavorite extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'post_id'];
+
+    /**
+     * Get the user that owns the UserFavorite
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post that owns the UserFavorite
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+}
