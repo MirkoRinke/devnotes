e464e653c9f45b5048950dd81bb7585a404ecc30
Mirko Rinke <mirkorinke@htp.com>
2025-03-09 02:02:56 +0100

    Refactor RegisterController to improve code readability and maintainability

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -6,17 +6,20 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\User;
-use Illuminate\Http\JsonResponse; // Import the JsonResponse class to use it in the controller example return new JsonResponse(['message' => 'User created successfully'], 201);
+use Illuminate\Http\JsonResponse;
 
-use App\Traits\ApiResponses; // Import the ApiResponses trait to use it in the controller example $this->successResponse($post, 'Post created successfully', 201);
+use App\Traits\ApiResponses; // example $this->successResponse($post, 'Post created successfully', 201);
 
-use Illuminate\Validation\ValidationException; // Import the ValidationException class
+use Illuminate\Validation\ValidationException;
 
-class RegisterController extends Controller{
+class RegisterController extends Controller {
 
-    use ApiResponses; // Use the ApiResponses trait in the controller
+    /**
+     *  The traits used in the controller
+     */
+    use ApiResponses;
 
-   /**
+    /**
      * Register a new user
      *
      * @param Request $request
@@ -24,20 +27,21 @@ class RegisterController extends Controller{
      */
     public function register(Request $request): JsonResponse {
         try {
-            $validatedData = $request->validate([
-                'name' => 'required|string|max:255',
-                'email' => 'required|string|email|unique:users,email',
-                'password' => 'required|string|min:8|confirmed',
-            ],         
-            $this->getValidationMessages()
+            $validatedData = $request->validate(
+                [
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|unique:users,email',
+                    'password' => 'required|string|min:8|confirmed',
+                ],
+                $this->getValidationMessages()
             );
-    
+
             $user = User::create([
                 'name' => $validatedData['name'],
                 'email' => $validatedData['email'],
                 'password' => bcrypt($validatedData['password']),
             ]);
-    
+
             return $this->successResponse($user, 'User created successfully', 201);
         } catch (ValidationException $e) {
             return $this->errorResponse('Validation failed', $e->errors(), 422);
