0d203386aac3b00171235fd276de926b0856d297
Mirko Rinke <mirkorinke@htp.com>
2025-03-10 18:49:05 +0100

    Refactor FavoriteController to improve code clarity and remove unnecessary comments

--- app/Http/Controllers/Api/FavoriteController.php
+++ app/Http/Controllers/Api/FavoriteController.php
@@ -7,10 +7,6 @@ use Illuminate\Http\Request;
 use App\Models\UserFavorite;
 use App\Models\Post;
 
-use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-
-
 use App\Traits\ApiResponses; // example return $this->successResponse($posts, 'Posts retrieved successfully', 200);
 use App\Traits\ApiSorting;  // example $query = $this->sort(request(), $query, ['id', 'title', 'language', 'category', 'status']);
 use App\Traits\ApiFiltering; // example $query = $this->filter(request(), $query, ['title', 'language', 'category', 'status']);
@@ -18,7 +14,7 @@ use App\Traits\SelectableAttributes; // example $this->selectAttributes($request
 use App\Traits\ApiPagination; // example $this->getPerPage($request, $query, 10);
 use App\Traits\QueryBuilder; // example $this->buildQuery($request, $query, $methods);
 
-
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\JsonResponse;
 
@@ -49,12 +45,10 @@ class FavoriteController extends Controller {
 
         $query = $this->buildQuery($request, $query, $this->methods);
 
-        // Check if the query is an instance of JsonResponse and return the response
         if ($query instanceof JsonResponse) {
             return $query;
         }
 
-        // Check if the query is empty and return a response
         if ($query->isEmpty()) {
             return $this->successResponse($query, 'No favorites found', 200);
         }
@@ -70,19 +64,15 @@ class FavoriteController extends Controller {
             $user = $request->user();
             $post = Post::findOrFail($postId);
 
-            // Check if the post is already in favorites
             $exists = UserFavorite::where('user_id', $user->id)->where('post_id', $post->id)->exists();
 
             if (!$exists) {
-                // Add the post to favorites
                 $favorite = UserFavorite::create(['user_id' => $user->id, 'post_id' => $post->id]);
 
-                // Increment the favorite count for the post
                 $post->increment('favorite_count');
 
                 return $this->successResponse($favorite, 'Post successfully added to favorites', 201);
             } else {
-                // Return the favorite if the post is already in favorites
                 $favorite = UserFavorite::where('user_id', $user->id)->where('post_id', $post->id)->first();
 
                 return $this->successResponse($favorite, 'Post already in favorites', 200);
@@ -108,10 +98,9 @@ class FavoriteController extends Controller {
 
             $this->authorize('delete', $favorite);
 
-            // Decrement the favorite count for the post and then delete the favorite
             $post->decrement('favorite_count');
-
             $favorite->delete();
+
             return $this->successResponse(null, 'Post successfully removed from favorites', 200);
         } catch (ModelNotFoundException $e) {
             return $this->errorResponse('Post not found', 'POST_NOT_FOUND', 404);
