3e3322cad99cf85068a8c5c8027752f8e09ed767
Mirko Rinke <mirkorinke@htp.com>
2025-03-10 19:42:39 +0100

    Refactor UserApiController to improve code organization by moving validation rules to a private property and removing unnecessary comments

--- app/Http/Controllers/Api/UserApiController.php
+++ app/Http/Controllers/Api/UserApiController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
 
 use App\Traits\ApiResponses; // example $this->successResponse($users, 'Users retrieved successfully', 200);
 use App\Traits\ApiSorting; // example $this->sort($request, $query, [ 'id','name', 'email']);
@@ -15,12 +15,12 @@ use App\Traits\SelectableAttributes; // example $this->selectAttributes($request
 use App\Traits\ApiPagination; // example $this->getPerPage($request, $query, 10);
 use App\Traits\QueryBuilder; // example $this->buildQuery($request, $query, $methods);
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
 use Exception;
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+use Illuminate\Auth\Access\AuthorizationException;
 
 class UserApiController extends Controller {
 
@@ -28,6 +28,12 @@ class UserApiController extends Controller {
      *  The traits used in the controller
      */
     use ApiResponses, ApiSorting, ApiFiltering, SelectableAttributes, ApiPagination, QueryBuilder, AuthorizesRequests;
+
+    private $validationRules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|unique:users,email',
+        'password' => 'required|string|min:8|confirmed',
+    ];
 
     /**
      * The methods array contains the methods that are used in the buildQuery method
@@ -46,23 +52,18 @@ class UserApiController extends Controller {
         try {
             $this->authorize('viewAny', User::class);
 
-            // Check if there are no users in the database and return a response message
             if (User::count() === 0) {
                 return $this->successResponse([], 'No users exist in the database', 200);
             }
 
             $query = User::query();
 
-
-            // Build the query based on the request and return JsonResponse|Collection|LengthAwarePaginator
             $query = $this->buildQuery($request, $query, $this->methods);
 
-            // Check if the query is an instance of JsonResponse and return the response
             if ($query instanceof JsonResponse) {
                 return $query;
             }
 
-            // Check if the query is empty after filtering and return the response
             if ($query->isEmpty()) {
                 return $this->successResponse($query, 'No users found with the given filters', 200);
             }
@@ -82,10 +83,8 @@ class UserApiController extends Controller {
         try {
             $query = User::query()->where('id', $id);
 
-            // Select the user attributes based on the request select array
             $query = $this->select($request, $query, $this->methods['select']);
 
-            // Check return value of the selectAttributes method and return the response if status code is 400
             if ($query instanceof JsonResponse && $query->getStatusCode() === 400) {
                 return $query;
             }
@@ -93,7 +92,6 @@ class UserApiController extends Controller {
             // Need this because the select method returns only the query object
             $user = $query->firstOrFail();
 
-            // Check if the user can view the user and return the response
             $this->authorize('view', $user);
 
             return $this->successResponse($user, 'User retrieved successfully', 200);
@@ -111,15 +109,10 @@ class UserApiController extends Controller {
         try {
             $user = User::findOrFail($id);
 
-            // Check if the user can update the user and return the response
             $this->authorize('update', $user);
 
             $validatedData = $request->validate(
-                [
-                    'name' => 'sometimes|string|max:255',
-                    'email' => 'sometimes|string|email|unique:users,email,' . $user->id,
-                    'password' => 'sometimes|string|min:8|confirmed',
-                ],
+                $this->validationRules,
                 $this->getValidationMessages()
             );
 
@@ -146,7 +139,6 @@ class UserApiController extends Controller {
         try {
             $user = User::findOrFail($id);
 
-            // Check if the user can delete the user and return the response
             $this->authorize('delete', $user);
 
             $user->delete();
