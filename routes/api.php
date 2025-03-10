1bbf70a0d759c6385d3e352d27fff1e8915c8368
Mirko Rinke <mirkorinke@htp.com>
2025-03-10 18:31:51 +0100

    Refactor API routes for improved readability and organization

--- routes/api.php
+++ routes/api.php
@@ -3,60 +3,61 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Api\AuthController; // Import the controller to use it in the routes file
-use App\Http\Controllers\Api\RegisterController; // Import the controller to use it in the routes file
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\AuthController;
 
-use App\Http\Controllers\Api\UserApiController; // Import the controller to use it in the routes file
-use App\Http\Controllers\Api\PostApiController; // Import the controller to use it in the routes file
-
-use App\Http\Controllers\Api\FavoriteController; // Import the controller to use it in the routes file
-
-use App\Http\Controllers\API\UserProfileController; // Import the controller to use it in the routes file
+use App\Http\Controllers\Api\UserApiController;
+use App\Http\Controllers\Api\PostApiController;
+use App\Http\Controllers\Api\FavoriteController;
+use App\Http\Controllers\API\UserProfileController;
 
 Route::middleware('throttle:api')->group(function () {
 
-    //! Route to register a new user
+    //! Route for users
 
-    // Public routes that do not require authentication route to register a new user
+    // Route to register a new user do not require authentication
     Route::post('/register', [RegisterController::class, 'register']);
 
-    //! Route to login and get a token
-
-    // Public routes that do not require authentication route to login and get a token
+    // Route to login and get a token do not require authentication
     Route::post('/login', [AuthController::class, 'login']);
 
-    //! Route to logout
-    // Protected routes that require authentication
+    // Route to logout and revoke token you need to be authenticated
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/tokens', [AuthController::class, 'getUserTokens']);
+        Route::delete('/tokens/{id}', [AuthController::class, 'revokeToken']);
+        Route::delete('/tokens', [AuthController::class, 'revokeAllTokens']);
     });
 
-
-    //! Route to all users
-    // Protected routes that require authentication and access control
+    // Route to get all users you need to be authenticated protected by sanctum and Policies
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('users', UserApiController::class);
     });
 
-    //! Route to posts 
-    // Public routes that do not require authentication
+
+    //! Route for posts
+
+    // Route to get all posts and a single post do not require authentication
     Route::get('/posts', [PostApiController::class, 'index']);
     Route::get('/posts/{post}', [PostApiController::class, 'show']);
 
-    // Protected routes that require authentication
+    // Route to create, update and delete a post you need to be authenticated protected by sanctum and Policies
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('posts', PostApiController::class)->except(['index', 'show']);
     });
 
-    //! Route to favorites
-    // Protected routes that require authentication and access control
+    //! Route for favorites
+
+    // Route to add, remove a favorite and get all favorites you need to be authenticated protected by sanctum and Policies
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/posts/{post}/favorites', [FavoriteController::class, 'addFavorite']);
         Route::delete('/posts/{post}/favorites', [FavoriteController::class, 'removeFavorite']);
         Route::get('/user/favorites', [FavoriteController::class, 'getFavorites']);
     });
 
-    //! Route to user profiles
+    //! Route for user profiles
+
+    // Route to get all user profiles you need to be authenticated protected by sanctum and Policies
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('user_profiles', UserProfileController::class);
     });
