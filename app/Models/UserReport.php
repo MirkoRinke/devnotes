bf2b5c285c2cc378c74777632f8384e33ece8f0d
Mirko Rinke <mirkorinke@htp.com>
2025-03-11 01:21:10 +0100

    Create UserReport model to handle user post reports

---
+++ app/Models/UserReport.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserReport extends Model {
+
+    /**
+     *  The traits used in the controller
+     */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id', 'post_id'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'user_id' => 'integer',
+        'post_id' => 'integer',
+    ];
+
+    /**
+     * Get the user that owns the UserReport
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post that owns the UserReport
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function post() {
+        return $this->belongsTo(Post::class);
+    }
+}
