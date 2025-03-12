ee860a89e99cbcbc7879684461af6f0f623a048d
Mirko Rinke <mirkorinke@htp.com>
2025-03-12 01:09:36 +0100

    Remove redundant methods from PostPolicy for improved clarity

--- app/Policies/PostPolicy.php
+++ app/Policies/PostPolicy.php
@@ -8,30 +8,6 @@ use Illuminate\Auth\Access\Response;
 
 class PostPolicy {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(): bool {
-        // Every user can view all posts
-        return true;
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(): bool {
-        // Every user can view a post
-        return true;
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool {
-        // Only authenticated users can create a post
-        return true;
-    }
-
-    /**
      * Determine whether the user can update the model.
      */
     public function update(User $user, Post $post): bool {
