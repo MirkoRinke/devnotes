18f2da7d2e6c6f045438bee27d1b81f2614ad1c6
Mirko Rinke <mirkorinke@htp.com>
2025-03-14 00:21:10 +0100

    Add ApiKey model to manage API key attributes and their types

---
+++ app/Models/ApiKey.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiKey extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'key',
+        'active',
+        'last_used_at'
+    ];
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'active' => 'boolean',
+        'last_used_at' => 'datetime',
+    ];
+}
