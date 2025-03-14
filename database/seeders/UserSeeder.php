8fc91786c2d83e81eeae967b55b26d5561fa6671
Mirko Rinke <mirkorinke@htp.com>
2025-03-14 21:14:57 +0100

    Add UserSeeder to create admin and regular users for database seeding

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void {
+        // Create an admin user
+        User::create([
+            'name' => 'Max Mustermann1',
+            'email' => 'max@example1.com',
+            'password' => Hash::make('sicheresPasswort123'),
+            'role' => 'admin',
+        ]);
+
+        // Create multiple regular users with incrementing names/emails
+        for ($i = 2; $i <= 10; $i++) {
+            User::create([
+                'name' => "Max Mustermann{$i}",
+                'email' => "max@example{$i}.com",
+                'password' => Hash::make('sicheresPasswort123'),
+                'role' => 'user',
+            ]);
+        }
+    }
+}
