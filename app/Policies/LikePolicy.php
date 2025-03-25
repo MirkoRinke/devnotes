b6823b56b5f3b1cc027663d8b8a9e0b6cc2f387d
Mirko Rinke <mirkorinke@htp.com>
2025-03-25 19:13:57 +0100

    Add viewAny method to LikePolicy to allow admin users to view all likes

--- app/Policies/LikePolicy.php
+++ app/Policies/LikePolicy.php
@@ -7,6 +7,18 @@ use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
 class LikePolicy {
+
+    /**
+     * Determine whether the user can view any likes.
+     */
+    public function viewAny(User $user): bool {
+        if ($user->role === 'admin') {
+            // This user is an admin
+            return true;
+        }
+        return false;
+    }
+
     /**
      * The user can only delete their own like.
      *
