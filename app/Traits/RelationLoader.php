47e86d3028cff05e9d465a6f1652e2b6728d6910
Mirko Rinke <mirkorinke@htp.com>
2025-03-26 03:04:19 +0100

    Refactor RelationLoader trait to support loading multiple relations based on selected columns

--- app/Traits/RelationLoader.php
+++ app/Traits/RelationLoader.php
@@ -7,16 +7,14 @@ use Illuminate\Database\Eloquent\Builder;
 
 trait RelationLoader {
     /**
-     * Load a relation only if specific columns are selected
-     * 
+     * Load multiple relations based on selected columns
+     *
      * @param Request $request
      * @param Builder $query
-     * @param string $relation Name of the relation to load
-     * @param string $foreignKey Key that must be present in selected columns
-     * @param array $columns Columns to select from the related model
+     * @param array $relationConfig Array of relation configurations
      * @return Builder
      */
-    protected function loadRelation(Request $request, Builder $query, string $relation, string $foreignKey, array $columns = ['*']) {
+    protected function loadRelations(Request $request, Builder $query, array $relationConfig) {
         $selectParameter = $request->input('select');
         $selectArray = [];
 
@@ -26,12 +24,38 @@ trait RelationLoader {
             $selectArray = $selectParameter;
         }
 
-        if (empty($selectArray) || in_array($foreignKey, $selectArray)) {
-            $query = $query->with([$relation => function ($query) use ($columns) {
-                $query->select($columns);
-            }]);
+        foreach ($relationConfig as $config) {
+            $relation = $config['relation'];
+            $foreignKey = $config['foreignKey'];
+            $columns = $config['columns'] ?? ['*'];
+
+            if (empty($selectArray) || in_array($foreignKey, $selectArray)) {
+                $query = $query->with([$relation => function ($query) use ($columns) {
+                    $query->select($columns);
+                }]);
+            }
         }
 
         return $query;
+    }
+
+    /**
+     * Load a single relation based on selected columns
+     *
+     * @param Request $request
+     * @param Builder $query
+     * @param string $relation
+     * @param string $foreignKey
+     * @param array $columns
+     * @return Builder
+     */
+    protected function loadRelation(Request $request, Builder $query, string $relation, string $foreignKey, array $columns = ['*']) {
+        return $this->loadRelations($request, $query, [
+            [
+                'relation' => $relation,
+                'foreignKey' => $foreignKey,
+                'columns' => $columns
+            ]
+        ]);
     }
 }
