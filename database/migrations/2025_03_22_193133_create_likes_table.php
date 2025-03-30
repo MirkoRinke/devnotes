e3ac843e9f3b8f28d36008bd63f49a6eea969114
Mirko Rinke <mirkorinke@htp.com>
2025-03-30 23:34:25 +0200

    refactor(migration): enhance likes table schema with improved structure and clarity

--- database/migrations/2025_03_22_193133_create_likes_table.php
+++ database/migrations/2025_03_22_193133_create_likes_table.php
@@ -10,12 +10,15 @@ return new class extends Migration {
      */
     public function up(): void {
         Schema::create('likes', function (Blueprint $table) {
+            // Default
             $table->id();
+            $table->timestamps();
+
+            // Basic
             $table->foreignId('user_id')->constrained()->onDelete('cascade'); // User who created the like
             $table->morphs('likeable');  // Polymorphic relationship (for Post|Comment etc.)
-            $table->string('type')->nullable(); // Simple type (post, comment)
             $table->unique(['user_id', 'likeable_id', 'likeable_type']); // A user can only like an entity once
-            $table->timestamps();
+            $table->string('type')->nullable(); // Simple type (post, comment)
         });
     }
 
