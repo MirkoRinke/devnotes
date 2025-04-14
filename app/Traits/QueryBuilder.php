68e3f2800af8308ca466babb4d55f38d1b35531c
Mirko Rinke <mirkorinke@htp.com>
2025-04-14 22:48:47 +0200

    feat(QueryBuilder): add external video and resource loading settings to query configuration

--- app/Traits/QueryBuilder.php
+++ app/Traits/QueryBuilder.php
@@ -72,7 +72,7 @@ trait QueryBuilder {
                 // Basic
                 ...['user_id', 'display_name', 'public_email', 'website', 'avatar_path', 'is_public', 'location', 'skills', 'biography', 'social_links', 'contact_channels'],
                 // Settings
-                ...['auto_load_external_images', 'external_images_temp_until'],
+                ...['auto_load_external_images', 'external_images_temp_until', 'auto_load_external_videos', 'external_videos_temp_until', 'auto_load_external_resources', 'external_resources_temp_until'],
                 // Counts
                 ...['reports_count'],
             ],
