c8fc0ac5084c5ea267109878bfe4074d66670cea
Mirko Rinke <mirkorinke@htp.com>
2025-04-14 23:23:06 +0200

    feat(PostSeeder): add external source previews for multiple example posts

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -21,6 +21,38 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://svelte.dev/docs#run-time-store", "https://svelte-tutorial.net"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://svelte.dev/docs#run-time-store",
+                    "type" => "resources",
+                    "domain" => "svelte.dev"
+                ],
+                [
+                    "url" => "https://svelte-tutorial.net",
+                    "type" => "resources",
+                    "domain" => "svelte-tutorial.net"
+                ]
+            ],
             'language' => "JavaScript",
             'category' => "Frontend-Entwicklung",
             'tags' => ["svelte", "store", "state-management"],
@@ -36,6 +68,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://laravel.com/docs/8.x/eloquent"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://laravel.com/docs/8.x/eloquent",
+                    "type" => "resources",
+                    "domain" => "laravel.com"
+                ]
+            ],
             'language' => "PHP",
             'category' => "Backend-Entwicklung",
             'tags' => ["laravel", "eloquent", "orm"],
@@ -51,6 +110,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://v3.vuejs.org/guide/composition-api-introduction.html"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://v3.vuejs.org/guide/composition-api-introduction.html",
+                    "type" => "resources",
+                    "domain" => "v3.vuejs.org"
+                ]
+            ],
             'language' => "JavaScript",
             'category' => "Frontend-Entwicklung",
             'tags' => ["vue", "composition-api"],
@@ -66,6 +152,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://reactjs.org/docs/components-and-props.html"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://reactjs.org/docs/components-and-props.html",
+                    "type" => "resources",
+                    "domain" => "reactjs.org"
+                ]
+            ],
             'language' => "JavaScript",
             'category' => "Frontend-Entwicklung",
             'tags' => ["react", "functional-components"],
@@ -81,6 +194,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://expressjs.com/en/starter/hello-world.html"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://expressjs.com/en/starter/hello-world.html",
+                    "type" => "resources",
+                    "domain" => "expressjs.com"
+                ]
+            ],
             'language' => "JavaScript",
             'category' => "Backend-Entwicklung",
             'tags' => ["node.js", "restful-api"],
@@ -96,6 +236,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://docs.docker.com/get-started/overview/"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://docs.docker.com/get-started/overview/",
+                    "type" => "resources",
+                    "domain" => "docs.docker.com"
+                ]
+            ],
             'language' => "Shell",
             'category' => "DevOps",
             'tags' => ["docker", "containerization"],
@@ -111,6 +278,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell",
+                    "type" => "resources",
+                    "domain" => "git-scm.com"
+                ]
+            ],
             'language' => "Shell",
             'category' => "DevOps",
             'tags' => ["git", "branching"],
@@ -126,6 +320,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://www.python.org/doc/"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.python.org/doc/",
+                    "type" => "resources",
+                    "domain" => "www.python.org"
+                ]
+            ],
             'language' => "Python",
             'category' => "Data Science",
             'tags' => ["python", "data-science"],
@@ -141,6 +362,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://docs.aws.amazon.com/s3/index.html"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://docs.aws.amazon.com/s3/index.html",
+                    "type" => "resources",
+                    "domain" => "docs.aws.amazon.com"
+                ]
+            ],
             'language' => "Shell",
             'category' => "Cloud Computing",
             'tags' => ["aws", "s3", "cloud"],
@@ -156,6 +404,33 @@ class PostSeeder extends Seeder {
             'images' => ["https://picsum.photos/200", "https://picsum.photos/200"],
             'videos' => ["https://www.youtube.com/watch?v=dQw4w9WgXcQ", "https://www.youtube.com/watch?v=dQw4w9WgXcQ"],
             'resources' => ["https://graphql.org/learn/"],
+            'external_source_previews' => [
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://picsum.photos/200",
+                    "type" => "images",
+                    "domain" => "picsum.photos"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                    "type" => "videos",
+                    "domain" => "www.youtube.com"
+                ],
+                [
+                    "url" => "https://graphql.org/learn/",
+                    "type" => "resources",
+                    "domain" => "graphql.org"
+                ]
+            ],
             'language' => "GraphQL",
             'category' => "Backend-Entwicklung",
             'tags' => ["graphql", "query-language"],
