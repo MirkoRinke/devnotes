c1b19f8c0eec5c60912c3e428c584d0a57bb937c
Mirko Rinke <mirkorinke@htp.com>
2025-04-19 02:07:03 +0200

    feat(migration): create user_followers table with foreign key constraints

---
+++ database/migrations/2025_04_18_190207_create_user_followers_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('user_followers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->unique(['user_id', 'follower_id'], 'user_follower_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('user_followers');
+    }
+};
