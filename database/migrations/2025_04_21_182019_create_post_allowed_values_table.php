2f95eca7b116ae8a19015c965470f5deec1ea44c
Mirko Rinke <mirkorinke@htp.com>
2025-04-21 21:46:21 +0200

    feat(migration): create post_allowed_values table with necessary fields and foreign key constraint

---
+++ database/migrations/2025_04_21_182019_create_post_allowed_values_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('post_allowed_values', function (Blueprint $table) {
+            // Default
+            $table->id();
+            $table->timestamps();
+
+            // Basic
+            $table->string('name');
+            $table->string('type');
+            $table->unique(['name', 'type']);
+            $table->string('created_by_role')->default('system'); // Assuming 'system' is the default role for the system user
+            $table->unsignedBigInteger('created_by_user_id')->default(2); // Assuming 2 is the ID of the system user
+            $table->foreign('created_by_user_id')->references('id')->on('users'); // Foreign key reference to users table
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('post_allowed_values');
+    }
+};
