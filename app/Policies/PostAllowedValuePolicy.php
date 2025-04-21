ce6b807f332146751e372d01084e3e8931bb46af
Mirko Rinke <mirkorinke@htp.com>
2025-04-22 00:48:10 +0200

    fix(PostAllowedValuePolicy): implement access control for model actions based on user roles

--- app/Policies/PostAllowedValuePolicy.php
+++ app/Policies/PostAllowedValuePolicy.php
@@ -8,37 +8,44 @@ use Illuminate\Auth\Access\Response;
 
 class PostAllowedValuePolicy {
     /**
+     * Determine whether the user can access the admin area.
+     */
+    private function accessAdminArea(User $user): bool {
+        return $user->role === 'admin' || $user->role === 'moderator';
+    }
+
+    /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool {
-        return false;
+        return $this->accessAdminArea($user);
     }
 
     /**
      * Determine whether the user can view the model.
      */
     public function view(User $user): bool {
-        return false;
+        return $this->accessAdminArea($user);
     }
 
     /**
      * Determine whether the user can create models.
      */
     public function create(User $user): bool {
-        return false;
+        return $this->accessAdminArea($user);
     }
 
     /**
      * Determine whether the user can update the model.
      */
     public function update(User $user): bool {
-        return false;
+        return $this->accessAdminArea($user);
     }
 
     /**
      * Determine whether the user can delete the model.
      */
     public function delete(User $user): bool {
-        return false;
+        return $this->accessAdminArea($user);
     }
 }
