a1b1bc38e14c5c38a5abe67aaca1906285cd3385
MirkoRinke <mirkorinke@htp.com>
2025-05-07 02:36:40 +0200

    refactor(UserLike): consolidate hidden attributes definition for clarity

--- app/Models/UserLike.php
+++ app/Models/UserLike.php
@@ -31,6 +31,16 @@ class UserLike extends Model {
     ];
 
     /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'user',
+        'likeable',
+    ];
+
+    /**
      * The attributes that should be cast to native types.
      *
      * @var array
