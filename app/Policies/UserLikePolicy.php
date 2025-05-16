11b344468c82398a30419b8ff5ce39703e05339c
MirkoRinke <mirkorinke@htp.com>
2025-05-17 00:57:48 +0200

    refactor(UserLikePolicy): implement PolicyChecks trait for consistent permissions and enhance documentation.

--- app/Policies/UserLikePolicy.php
+++ app/Policies/UserLikePolicy.php
@@ -4,16 +4,32 @@ namespace App\Policies;
 
 use App\Models\UserLike;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
+use App\Traits\PolicyChecks;
+
+/**
+ * Class UserLikePolicy
+ * 
+ * This policy class handles authorization for the UserLike model.
+ * It uses the PolicyChecks trait for common checks.
+ */
 class UserLikePolicy {
 
     /**
+     * The traits used in the policy
+     */
+    use PolicyChecks;
+
+    /**
      * Determine whether the user can view any likes.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('viewAny', UserLike::class); 
      */
     public function viewAny(User $user): bool {
-        if ($user->role === 'admin') {
-            // This user is an admin
+        if ($this->isAdmin($user)) {
             return true;
         }
         return false;
@@ -22,11 +38,13 @@ class UserLikePolicy {
     /**
      * The user can only delete their own like.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\UserLike  $commentLike
+     * @param User $user
+     * @param UserLike $userLike
      * @return bool
+     * 
+     * @example | $this->authorize('delete', $userLike);
      */
-    public function delete(User $user, UserLike $commentLike): bool {
-        return $user->id === $commentLike->user_id;
+    public function delete(User $user, UserLike $userLike): bool {
+        return $this->isOwner($user, $userLike);
     }
 }
