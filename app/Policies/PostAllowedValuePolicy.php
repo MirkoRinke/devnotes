7497438cc68719732f1972d5dafd76759e9b6a1c
MirkoRinke <mirkorinke@htp.com>
2025-05-17 00:57:28 +0200

    refactor(PostAllowedValuePolicy): implement PolicyChecks trait for consistent permissions and enhance documentation.

--- app/Policies/PostAllowedValuePolicy.php
+++ app/Policies/PostAllowedValuePolicy.php
@@ -2,50 +2,80 @@
 
 namespace App\Policies;
 
-use App\Models\PostAllowedValue;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
+use App\Traits\PolicyChecks;
+
+/**
+ * Class PostAllowedValuePolicy
+ * 
+ * This policy class handles authorization for the PostAllowedValue model.
+ * It uses the PolicyChecks trait for common checks.
+ */
 class PostAllowedValuePolicy {
+
     /**
-     * Determine whether the user can access the admin area.
+     * The traits used in the policy
      */
-    private function accessAdminArea(User $user): bool {
-        return $user->role === 'admin' || $user->role === 'moderator';
-    }
+    use PolicyChecks;
 
     /**
      * Determine whether the user can view any models.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('viewAny', PostAllowedValue::class);
      */
     public function viewAny(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can view the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('view', $postAllowedValue);
      */
     public function view(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can create models.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('create', PostAllowedValue::class);
      */
     public function create(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can update the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('update', $postAllowedValue);
      */
     public function update(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can delete the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('delete', $postAllowedValue);
      */
     public function delete(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 }
