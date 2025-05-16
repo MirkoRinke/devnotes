72e806b691fbfb783cd41cda3049ddaef5456dc7
MirkoRinke <mirkorinke@htp.com>
2025-05-17 00:57:11 +0200

    refactor(CriticalTermPolicy): implement PolicyChecks trait for consistent permissions and enhance documentation.

--- app/Policies/CriticalTermPolicy.php
+++ app/Policies/CriticalTermPolicy.php
@@ -2,50 +2,80 @@
 
 namespace App\Policies;
 
-use App\Models\CriticalTerm;
 use App\Models\User;
-use Illuminate\Auth\Access\Response;
 
+use App\Traits\PolicyChecks;
+
+/**
+ * Class CriticalTermPolicy
+ * 
+ * This policy class handles authorization for the CriticalTerm model.
+ * It uses the PolicyChecks trait for common checks.
+ */
 class CriticalTermPolicy {
+
     /**
-     * Determine whether the user can access the admin area.
+     * The traits used in the policy
      */
-    private function accessAdminArea(User $user): bool {
-        return $user->role === 'admin' || $user->role === 'moderator';
-    }
+    use PolicyChecks;
 
     /**
      * Determine whether the user can view any models.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('viewAny', CriticalTerm::class);
      */
     public function viewAny(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can view the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('view', $criticalTerm);
      */
     public function view(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can create models.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('create', CriticalTerm::class);
      */
     public function create(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can update the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('update', $criticalTerm);
      */
     public function update(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 
     /**
      * Determine whether the user can delete the model.
+     * 
+     * @param User $user
+     * @return bool
+     * 
+     * @example | $this->authorize('delete', $criticalTerm);
      */
     public function delete(User $user): bool {
-        return $this->accessAdminArea($user);
+        return $this->hasModeratorPrivileges($user);
     }
 }
