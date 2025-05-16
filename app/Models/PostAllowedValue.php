e3a4f3716d20a03b599ff3894fcccbb3463c7e72
MirkoRinke <mirkorinke@htp.com>
2025-05-16 20:39:46 +0200

    refactor(PostAllowedValue): enhance documentation and reorganize attributes for clarity

--- app/Models/PostAllowedValue.php
+++ app/Models/PostAllowedValue.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class PostAllowedValue extends Model {
+
+    /**
+     * The traits used in the model
+     */
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -12,17 +18,41 @@ class PostAllowedValue extends Model {
      * @var array
      */
     protected $fillable = [
+        // Default
+        'id',
+
+        // Basic
         'name',
         'type',
         'created_by_role',
         'created_by_user_id',
+
+        // Update info
+        'created_at',
+        'updated_at',
     ];
 
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
 
     /**
      * Get the user who created this post allowed value
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * 
+     * @example | $postAllowedValue->createdByUser // Access the related user
+     * @example | PostAllowedValue::with('createdByUser')->get() // Eager loading
      */
     public function createdByUser() {
         return $this->belongsTo(User::class, 'created_by_user_id');
