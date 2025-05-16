3b66a977b15d7104ad00af14582aa2dd4d881fd4
MirkoRinke <mirkorinke@htp.com>
2025-05-16 20:39:03 +0200

    refactor(ForbiddenName): reorganize attributes and enhance documentation for clarity

--- app/Models/ForbiddenName.php
+++ app/Models/ForbiddenName.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ForbiddenName extends Model {
+
+    /**
+     * The traits used in the model
+     */
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -12,16 +18,41 @@ class ForbiddenName extends Model {
      * @var array
      */
     protected $fillable = [
+        // Default
+        'id',
+
+        // Basic
         'name',
         'match_type',
         'created_by_role',
-        'created_by_user_id'
+        'created_by_user_id',
+
+        // Update info
+        'created_at',
+        'updated_at',
     ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [];
 
     /**
      * Get the user who created this forbidden name
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * 
+     * @example | $forbiddenName->creator // Access the creator of the forbidden name
+     * @example | ForbiddenName::with('creator')->get() // Eager loading
      */
     public function creator() {
         return $this->belongsTo(User::class, 'created_by_user_id');
