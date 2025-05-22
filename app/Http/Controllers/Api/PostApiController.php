c787463ed1f26f450fcf0ff21795cf221d1e73b7
MirkoRinke <mirkorinke@htp.com>
2025-05-22 20:50:59 +0200

    feat(PostApiController): integrate PostQuerySetup trait and use setupPostQuery from trait.

--- app/Http/Controllers/Api/PostApiController.php
+++ app/Http/Controllers/Api/PostApiController.php
@@ -18,6 +18,7 @@ use App\Traits\ApiInclude;
 use App\Traits\QueryBuilder;
 use App\Traits\RelationLoader;
 use App\Traits\FieldManager;
+use App\Traits\PostQuerySetup;
 
 use App\Services\ModerationService;
 use App\Services\ExternalSourceService;
@@ -37,7 +38,7 @@ class PostApiController extends Controller {
     /**
      *  The traits used in the controller
      */
-    use ApiResponses,  QueryBuilder, ApiInclude, RelationLoader, FieldManager, AuthorizesRequests;
+    use ApiResponses, QueryBuilder, ApiInclude, RelationLoader, FieldManager, AuthorizesRequests, PostQuerySetup;
 
 
     /**
@@ -121,42 +122,6 @@ class PostApiController extends Controller {
             'status' => ['sometimes', 'required', 'string', new ValidPostValue('status')],
         ];
         return $validationRulesUpdate;
-    }
-
-
-    /**
-     * Setup the post query
-     * This method is used to set up the query for the post
-     * It applies the access filters.
-     * It applies sorting, filtering, selecting, and pagination
-     * It also loads the relations for the post
-     * 
-     * @param Request $request
-     * @param $query
-     * @param $methods (string) The method to call for building the query
-     * @return mixed
-     * 
-     * @example | $query = $this->setupPostQuery($request, $query, 'buildQuery');
-     */
-    protected function setupPostQuery(Request $request, $query, string $methods): mixed {
-
-        $query = $this->applyAccessFilters($request, $query);
-
-        $relationKeyFields = $this->getRelationKeyFields($request, ['user' => 'user_id']);
-
-        $this->modifyRequestSelect($request, [...['id'], ...$relationKeyFields]);
-
-        $query = $this->loadRelations($request, $query, [
-            ['relation' => 'user', 'foreignKey' => 'user_id', 'columns' => $this->getRelationFieldsFromRequest($request, 'user', [], ['id', 'display_name', 'role', 'created_at', 'updated_at', 'is_banned', 'was_ever_banned', 'moderation_info'])],
-
-        ]);
-
-        $query = $this->$methods($request, $query, 'post');
-        if ($query instanceof JsonResponse) {
-            return $query;
-        }
-
-        return $query;
     }
 
 
