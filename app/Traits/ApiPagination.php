37c79d10d09ed2202452b370a027b2c6456ba055
MirkoRinke <mirkorinke@htp.com>
2025-05-31 00:44:18 +0200

    fix(ApiPagination): enforce pagination with size limits (10-100 items per page)

--- app/Traits/ApiPagination.php
+++ app/Traits/ApiPagination.php
@@ -25,29 +25,32 @@ trait ApiPagination {
      * 
      * @param Request $request
      * @param Builder $query
-     * @param int $default
+     * @param int $perPage
      * @return JsonResponse|Collection|LengthAwarePaginator
      * 
      * @example | $this->getPerPage($request, $query, (int) $config);
      */
-    public function getPerPage(Request $request, Builder $query, int $default = 10): JsonResponse|Collection|LengthAwarePaginator {
-        // Get the page and per_page parameters from the request
-        $page = $request->get('page');
+    public function getPerPage(Request $request, Builder $query, int $perPage = 10): JsonResponse|Collection|LengthAwarePaginator {
+        // Get the page parameter from the request or default to 1
+        $page = $request->get('page', 1);
 
-        if ($page) {
-            // Get the per_page parameter from the request
-            $perPage = $request->get('per_page', $default);
-            // Validate the page and per_page parameters
-            if (!is_numeric($page) || $page < 1) {
-                return $this->errorResponse('The page parameter must be a number greater than 0', ['page' => 'INVALID_PAGE_NUMBER'], 400);
-            }
-            // Validate the per_page parameter
-            if (!is_numeric($perPage) || $perPage < 1) {
-                return $this->errorResponse('The per_page parameter must be a number greater than 0', ['per_page' => 'INVALID_PER_PAGE_NUMBER'], 400);
-            }
-            // Paginate the query results and return the results to the client 
-            return $query->paginate((int) $perPage);
+        // Get the per_page parameter from the request
+        $perPage = $request->get('per_page', $perPage);
+
+        $perPage = $perPage < 1 ? 10 : $perPage; // If perPage is less than 1, set it to 10
+        $perPage = $perPage > 100 ? 100 : $perPage; // If perPage is greater than 100, set it to 100
+
+        // Validate the page parameter
+        if (!is_numeric($page) || $page < 1) {
+            return $this->errorResponse('The page parameter must be a number greater than 0', ['page' => 'INVALID_PAGE_NUMBER'], 400);
         }
-        return $query->get();
+
+        // Validate the per_page parameter
+        if (!is_numeric($perPage) || $perPage < 1) {
+            return $this->errorResponse('The per_page parameter must be a number greater than 0', ['per_page' => 'INVALID_PER_PAGE_NUMBER'], 400);
+        }
+
+        // Always paginate the query results
+        return $query->paginate((int) $perPage);
     }
 }
