b64c29e5f4786196ac8be1cd7c2de5c6310d2bbd
MirkoRinke <mirkorinke@htp.com>
2025-06-05 02:57:50 +0200

    refactor(CreatePostsTable): standardize default values for post_type and status, remove tags column

--- database/migrations/2025_02_26_191557_create_posts_table.php
+++ database/migrations/2025_02_26_191557_create_posts_table.php
@@ -24,10 +24,9 @@ return new class extends Migration {
             $table->json('external_source_previews')->nullable();
             $table->json('language'); // programming language used (e.g., HTML, CSS, JavaScript, etc.)
             $table->string('category'); // category of the post (e.g., Frontend, Backend, etc.)
-            $table->string('post_type')->default('snippet'); // snippet, tutorial, feedback, showcase, question, etc.
+            $table->string('post_type')->default('Snippet'); // Snippet, Tutorial, Feedback, Showcase, Question, etc.
             $table->json('technology')->nullable();; // technology used (e.g., Angular, React, etc.)
-            $table->json('tags');
-            $table->string('status')->default('draft'); // status of the post (e.g., draft, private, published, archived)
+            $table->string('status')->default('Draft'); // status of the post (e.g., Draft, Private, Published, Archived)
 
             // Counts
             $table->integer('favorite_count')->default(0);
