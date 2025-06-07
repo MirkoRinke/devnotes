3eac42463b3275a5e6d85cb98b396296c671b6b7
MirkoRinke <mirkorinke@htp.com>
2025-06-07 20:30:08 +0200

    refactor(FavoriteSeeder): enhance seeding logic to allow up to 5 favorites per user and improve code clarity

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -10,27 +10,32 @@ use App\Models\Post;
 use App\Models\UserFavorite;
 
 class FavoriteSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      */
     public function run(): void {
-        // Get all user IDs from the database
+        $this->command->info('Seeding favorites...');
+
         $userIds = User::pluck('id')->toArray();
 
-        // Process each user to create their favorites
         foreach ($userIds as $userId) {
-            // First get all posts that aren't owned by this user
+            /**
+             * Get all posts that aren't owned by this user
+             */
             $eligiblePosts = Post::where('user_id', '!=', $userId)->pluck('id')->toArray();
 
-            // Only proceed if there are eligible posts for this user
             if (count($eligiblePosts) > 0) {
-                // Randomly determine how many favorites this user will have (0-2)
-                // But limit to the number of available posts
-                $maxPossibleFavorites = min(2, count($eligiblePosts));
+
+                /**
+                 * Randomly select a number of favorites for this user
+                 * This number is between 0 and 5, but will never exceed the number of eligible posts.
+                 */
+                $maxPossibleFavorites = min(5, count($eligiblePosts));
                 $favoritesCount = rand(0, $maxPossibleFavorites);
 
-                // Skip users who won't have any favorites
                 if ($favoritesCount > 0) {
+
                     // Shuffle eligible post IDs to randomize selection
                     shuffle($eligiblePosts);
 
@@ -39,17 +44,10 @@ class FavoriteSeeder extends Seeder {
 
                     // Create favorites for each selected post
                     foreach ($selectedPosts as $postId) {
-                        // Use firstOrCreate to check and create in one step
-                        $favorite = UserFavorite::firstOrCreate(
-                            [
-                                'user_id' => $userId,
-                                'post_id' => $postId,
-                            ],
-                            [
-                                'created_at' => now(),
-                                'updated_at' => now()
-                            ]
-                        );
+                        $favorite = UserFavorite::firstOrCreate([
+                            'user_id' => $userId,
+                            'post_id' => $postId,
+                        ]);
 
                         // Only increment the favorite count if a new favorite was created
                         if ($favorite->wasRecentlyCreated) {
@@ -59,5 +57,6 @@ class FavoriteSeeder extends Seeder {
                 }
             }
         }
+        $this->command->info('Favorites seeded successfully!');
     }
 }
