558c6ae584d529e40b91be5a371b5e44b2b8285d
MirkoRinke <mirkorinke@htp.com>
2025-06-07 20:27:57 +0200

    refactor(ApiKeySeeder): improve seed process by adding informative messages during API key seeding

--- database/seeders/ApiKeySeeder.php
+++ database/seeders/ApiKeySeeder.php
@@ -15,10 +15,14 @@ class ApiKeySeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
+        $this->command->info('Seeding API keys...');
+
         $apiKey = new ApiKey();
         $apiKey->name = 'Development API Key';
         $apiKey->key = 'FQojPlIFCVzOBZWHbVmRMMy8jkOl0XlLM67lGD2E';
         $apiKey->active = true;
         $apiKey->save();
+
+        $this->command->info('API keys seeded successfully!');
     }
 }
