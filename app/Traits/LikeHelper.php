4d3854e05cf7655c7d610dd94d56efa6d3382c6e
MirkoRinke <mirkorinke@htp.com>
2025-06-19 01:58:50 +0200

    feat(LikeHelper): add LikeHelper trait to manage user liking functionality for posts and comments

---
+++ app/Traits/LikeHelper.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+use App\Models\Post;
+use App\Models\Comment;
+use App\Models\User;
+use App\Models\UserLike;
+
+trait LikeHelper {
+
+    /**
+     * Check if the user can like the content
+     * 
+     * @param mixed $user The authenticated user
+     * @param string $likeableType The type of entity to like (Post or Comment)
+     * @param mixed $likeable The likeable entity
+     * @param int $likeableId The ID of the entity to like
+     * @param string $simpleType The simple type of the entity (post or comment)
+     * @return JsonResponse|null
+     * 
+     * @example | $likeableResult = $this->checkIfUserCanLike($user, $likeableType, $likeable, $likeableId, $simpleType);
+     *            if ($likeableResult !== null) {
+     *              return $likeableResult;
+     *            }
+     */
+    private function checkIfUserCanLike($user, $likeableType, $likeable, $likeableId, $simpleType) {
+        if ($likeableType === Post::class && $likeable->user_id == $user->id) {
+            return $this->errorResponse('You cannot like your own post', 'CANNOT_LIKE_OWN_POST', 403);
+        } else if ($likeableType === Comment::class && $likeable->user_id == $user->id) {
+            return $this->errorResponse('You cannot like your own comment', 'CANNOT_LIKE_OWN_COMMENT', 403);
+        }
+
+        $existingLike = UserLike::where([
+            'user_id' => $user->id,
+            'likeable_id' => $likeableId,
+            'likeable_type' => $likeableType
+        ])->first();
+
+        if ($existingLike) {
+            return $this->errorResponse('You have already liked this ' . $simpleType, 'ALREADY_LIKED', 403);
+        }
+        return null;
+    }
+
+
+    /**
+     * Check if entities are liked by the current user
+     *
+     * @param User|null $user
+     * @param Builder|Collection|LengthAwarePaginator|Post|Comment $query
+     * @param string $modelType 'post' or 'comment'
+     * @return mixed
+     * 
+     * @example | $isLiked = $this->isLiked($user, $query, 'post');
+     */
+    public function isLiked($user, $query, string $modelType): Builder|Collection|LengthAwarePaginator|Post|Comment {
+        $className = $modelType === 'post' ? Post::class : Comment::class;
+
+        if ($query instanceof $className) {
+            if (!$user) {
+                $query->is_liked = false;
+                return $query;
+            }
+
+            $query->is_liked = $user->likes()
+                ->where('likeable_type', $className)
+                ->where('likeable_id', $query->id)
+                ->exists();
+
+            return $query;
+        }
+
+        $ids = $query->pluck('id')->toArray();
+        $likedItems = [];
+
+        if ($user && !empty($ids)) {
+            $likedItems = $user->likes()
+                ->where('likeable_type', $className)
+                ->whereIn('likeable_id', $ids)
+                ->pluck('likeable_id')
+                ->toArray();
+        }
+
+        $query->each(function ($item) use ($likedItems) {
+            $item->is_liked = in_array($item->id, $likedItems);
+        });
+
+        return $query;
+    }
+}
