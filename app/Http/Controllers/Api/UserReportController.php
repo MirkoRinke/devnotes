1c7c293c25cc342dd51e36a416103b2eddceec06
MirkoRinke <mirkorinke@htp.com>
2025-06-23 23:03:48 +0200

    refactor(UserReportController): integrate ReportHelper trait and remove unused methods for critical term checks and report validation

--- app/Http/Controllers/Api/UserReportController.php
+++ app/Http/Controllers/Api/UserReportController.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\Controller;
 use App\Models\UserReport;
 use App\Models\Post;
 use App\Models\Comment;
-use App\Models\CriticalTerm;
 use App\Models\UserProfile;
 
 use App\Traits\ApiResponses;
@@ -20,6 +19,7 @@ use App\Traits\CacheHelper;
 use App\Traits\RelationLoader;
 use App\Traits\ApiInclude;
 use App\Traits\FieldManager;
+use App\Traits\ReportHelper;
 
 use App\Services\SnapshotService;
 
@@ -34,7 +34,7 @@ class UserReportController extends Controller {
     /**
      *  The traits used in the controller
      */
-    use ApiResponses, QueryBuilder, CacheHelper, AuthorizesRequests, RelationLoader, ApiInclude, FieldManager;
+    use ApiResponses, QueryBuilder, CacheHelper, AuthorizesRequests, RelationLoader, ApiInclude, FieldManager, ReportHelper;
 
     /**
      * The services used in the controller
@@ -80,42 +80,6 @@ class UserReportController extends Controller {
     }
 
     /**
-     * Check for critical terms in the reason
-     * 
-     * @param string $reason The report reason to check
-     * @return int The severity level
-     * 
-     * @example | $this->checkCriticalTerms($reason);
-     */
-    private function checkCriticalTerms($reason) {
-        // Default severity if no critical terms are found
-        $defaultSeverity = 1;
-
-        if (empty($reason)) {
-            return $defaultSeverity;
-        }
-
-        // Generate a cache key for the critical terms
-        $cacheKey = $this->generateSimpleCacheKey('critical_terms');
-
-        // Get all critical terms from the database
-        // Cache the critical terms for 1 hour
-        $criticalTerms = $this->cacheData($cacheKey, 3600, function () {
-            return CriticalTerm::all();
-        });
-
-        // Check if any critical term is found in the reason
-        foreach ($criticalTerms as $name) {
-            if (stripos($reason, $name->name) !== false) {
-                return $name->severity;
-            }
-        }
-
-        return $defaultSeverity;
-    }
-
-
-    /**
      * Setup the query for user reports
      * 
      * @param Request $request
@@ -159,44 +123,6 @@ class UserReportController extends Controller {
             );
         }
         return $query;
-    }
-
-
-    /**
-     * Check if the user can report the entity
-     * 
-     * @param User $user The user who is reporting
-     * @param string $reportableType The type of the reportable entity (Post, UserProfile, Comment)
-     * @param mixed $reportable The reportable entity
-     * @param int $reportableId The ID of the reportable entity
-     * @param string $simpleType The simple type of the reportable entity (post, userProfile, comment)
-     * @return JsonResponse|null
-     * 
-     * @example | $validationResult = $this->reportValidationCheck($user, $reportableType, $reportable, $reportableId, $simpleType);
-     *            if ($validationResult !== null) {
-     *             return $validationResult;
-     *          }
-     */
-    protected function reportValidationCheck($user, $reportableType, $reportable, $reportableId, $simpleType) {
-        if ($reportableType === UserProfile::class && $reportable->user_id == $user->id) {
-            return $this->errorResponse('You cannot report yourself', 'CANNOT_REPORT_SELF', 403);
-        } else if ($reportableType === Post::class && $reportable->user_id == $user->id) {
-            return $this->errorResponse('You cannot report your own post', 'CANNOT_REPORT_OWN_POST', 403);
-        } else if ($reportableType === Comment::class && $reportable->user_id == $user->id) {
-            return $this->errorResponse('You cannot report your own comment', 'CANNOT_REPORT_OWN_COMMENT', 403);
-        }
-
-        $existingReport = UserReport::where([
-            'user_id' => $user->id,
-            'reportable_id' => $reportableId,
-            'reportable_type' => $reportableType
-        ])->first();
-
-        if ($existingReport) {
-            return $this->errorResponse('You have already reported this ' . $simpleType, 'ALREADY_REPORTED',  409);
-        }
-
-        return null;
     }
 
 
