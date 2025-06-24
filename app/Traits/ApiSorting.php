56634a9fe14ed89210d12e23c81c389c580fdb9e
MirkoRinke <mirkorinke@htp.com>
2025-06-24 18:42:29 +0200

    feat(ApiSorting): support multi-field sorting via sort parameter

--- app/Traits/ApiSorting.php
+++ app/Traits/ApiSorting.php
@@ -20,7 +20,7 @@ trait ApiSorting {
     use ApiResponses;
 
     /**
-     * Sort the query results based on the request
+     * Sort the query based on the request parameters.
      *
      * @param Request $request
      * @param Builder $query
@@ -30,29 +30,33 @@ trait ApiSorting {
      * @example | $this->sort($request, $query, (array)$config);
      */
     public function sort(Request $request, Builder $query, array $allowedColumns = []): JsonResponse|Builder {
-        // Get the page parameter from the request
         $sort = $request->get('sort');
 
-        // If the sort parameter is not set, return the query with default sorting
+        /**
+         * If the sort parameter is not set, return the query with default sorting
+         */
         if (!$sort) {
             return $query->orderBy('id', 'asc');
         }
 
-        if ($sort) {
-            // Get the order column from the request
-            $orderSettings = $request->get('sort', 'name');
-            // If the order column is not set, return an error response
+        if (is_string($sort)) {
+            $sort = explode(',', $sort);
+        }
+
+        foreach ($sort as $orderSettings) {
+
+            // Trim the hyphen if it exists at the start of the string
             $orderColumnName = str_starts_with($orderSettings, '-') ? substr($orderSettings, 1) : $orderSettings;
-            // If the order column is not set, return an error response
+
+            // Determine the order direction based on the presence of a leading hyphen
             $orderDirection = str_starts_with($orderSettings, '-') ? 'desc' : 'asc';
+
             // If the column is not allowed, return an error response
             if (!empty($allowedColumns) && !in_array($orderColumnName, $allowedColumns)) {
                 return $this->errorResponse('Invalid order column: ' . $orderColumnName, ['sort' => 'INVALID_ORDER_COLUMN'], 400);
             }
-            // If the column is allowed, sort the query
-            if (empty($allowedColumns) || in_array($orderColumnName, $allowedColumns)) {
-                return $query->orderBy($orderColumnName, $orderDirection);
-            }
+
+            $query->orderBy($orderColumnName, $orderDirection);
         }
 
         return $query;
