13025b09199210d96ba8b303df2018ffc359f43f
MirkoRinke <mirkorinke@htp.com>
2025-06-30 21:14:38 +0200

    fix(migration): ensure DB facade is imported and maintain auto-increment starting point for user_profiles

--- database/migrations/2025_03_07_144834_create_users_profiles_table.php
+++ database/migrations/2025_03_07_144834_create_users_profiles_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use Illuminate\Support\Facades\DB;
+
 return new class extends Migration {
     /**
      * Run the migrations.
@@ -42,6 +44,8 @@ return new class extends Migration {
             // Update info
             $table->timestamps();
         });
+
+        DB::statement('ALTER TABLE user_profiles AUTO_INCREMENT = 100;');
     }
 
     /**
