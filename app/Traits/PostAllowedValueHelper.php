6a115ff2d8341d98efb6d14518ab40bdb623c77e
MirkoRinke <mirkorinke@htp.com>
2025-07-14 21:47:48 +0200

    feat(PostAllowedValueHelper): add trait for managing post allowed values with utility methods

---
+++ app/Traits/PostAllowedValueHelper.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+/**
+ * PostAllowedValueHelper
+ *
+ * This trait provides helper methods for working with Post Allowed Values.
+ */
+trait PostAllowedValueHelper {
+
+    /**
+     * Check if the Post Allowed Value is used in any posts
+     * 
+     * @param string $name The name of the allowed value
+     * @param string $type The type of the allowed value (e.g., category, post_type, status, language, technology, tag)
+     * @return bool True if the value is in use, false otherwise
+     * 
+     * @example | $isInUse = $this->isPostAllowedValueInUse($name, $type)
+     */
+    protected function isPostAllowedValueInUse($name, $type) {
+        $isInUse = false;
+
+        switch ($type) {
+            case 'category':
+                $isInUse = Post::where('category', $name)->exists();
+                break;
+            case 'post_type':
+                $isInUse = Post::where('post_type', $name)->exists();
+                break;
+            case 'status':
+                $isInUse = Post::where('status', $name)->exists();
+                break;
+            case 'language':
+                $isInUse = Post::with('languages')->whereHas('languages', function ($query) use ($name) {
+                    $query->where('name', $name);
+                })->exists();
+                break;
+            case 'technology':
+                $isInUse = Post::with('technologies')->whereHas('technologies', function ($query) use ($name) {
+                    $query->where('name', $name);
+                })->exists();
+                break;
+            case 'tag':
+                $isInUse = Post::with('tags')->whereHas('tags', function ($query) use ($name) {
+                    $query->where('name', $name);
+                })->exists();
+                break;
+        }
+        return $isInUse;
+    }
+}
