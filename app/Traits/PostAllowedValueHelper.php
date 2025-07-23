c30b00488340dff01a122878c11c4728171ff0bc
MirkoRinke <mirkorinke@htp.com>
2025-07-23 02:55:05 +0200

    refactor(PostAllowedValueHelper): remove unused allowed value types from isPostAllowedValueInUse method

--- app/Traits/PostAllowedValueHelper.php
+++ app/Traits/PostAllowedValueHelper.php
@@ -17,7 +17,7 @@ trait PostAllowedValueHelper {
      * Check if the Post Allowed Value is used in any posts
      * 
      * @param string $name The name of the allowed value
-     * @param string $type The type of the allowed value (e.g., category, post_type, status, language, technology, tag)
+     * @param string $type The type of the allowed value (e.g., category, post_type, status)
      * @return bool True if the value is in use, false otherwise
      * 
      * @example | $isInUse = $this->isPostAllowedValueInUse($name, $type)
@@ -34,21 +34,6 @@ trait PostAllowedValueHelper {
                 break;
             case 'status':
                 $isInUse = Post::where('status', $name)->exists();
-                break;
-            case 'language':
-                $isInUse = Post::with('languages')->whereHas('languages', function ($query) use ($name) {
-                    $query->where('name', $name);
-                })->exists();
-                break;
-            case 'technology':
-                $isInUse = Post::with('technologies')->whereHas('technologies', function ($query) use ($name) {
-                    $query->where('name', $name);
-                })->exists();
-                break;
-            case 'tag':
-                $isInUse = Post::with('tags')->whereHas('tags', function ($query) use ($name) {
-                    $query->where('name', $name);
-                })->exists();
                 break;
         }
         return $isInUse;
