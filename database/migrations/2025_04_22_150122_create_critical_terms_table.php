44758052e53fe487c5ff9c7231f3a7e321babb8f
MirkoRinke <mirkorinke@htp.com>
2025-07-23 02:52:38 +0200

    refactor(migration): remove inline comments in create_critical_terms_table migration for cleaner code

--- database/migrations/2025_04_22_150122_create_critical_terms_table.php
+++ database/migrations/2025_04_22_150122_create_critical_terms_table.php
@@ -14,12 +14,12 @@ return new class extends Migration {
             $table->id();
 
             // Basic
-            $table->string('name')->unique(); // Use name not term for consistency
-            $table->string('language')->default('en'); // Assuming 'en' is the default language
-            $table->integer('severity')->default(1); // Assuming severity is an integer between 1 and 5
-            $table->string('created_by_role')->default('system'); // Assuming 'system' is the default role for the system user
-            $table->unsignedBigInteger('created_by_user_id')->default(2); // Assuming 2 is the ID of the system user
-            $table->foreign('created_by_user_id')->references('id')->on('users'); // Foreign key reference to users table
+            $table->string('name')->unique();
+            $table->string('language')->default('en');
+            $table->integer('severity')->default(1);
+            $table->string('created_by_role')->default('system');
+            $table->unsignedBigInteger('created_by_user_id')->default(2);
+            $table->foreign('created_by_user_id')->references('id')->on('users');
 
             // Update info
             $table->timestamps();
