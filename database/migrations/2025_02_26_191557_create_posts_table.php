cec79326f5d4a6919dd7228d779c17608567e126
MirkoRinke <mirkorinke@htp.com>
2025-08-21 22:41:13 +0200

    fix(posts migration): remove default values for post_type and status in posts table

--- database/migrations/2025_02_26_191557_create_posts_table.php
+++ database/migrations/2025_02_26_191557_create_posts_table.php
@@ -23,8 +23,8 @@ return new class extends Migration {
             $table->json('resources')->nullable();
             $table->json('external_source_previews')->nullable();
             $table->string('category'); // category of the post (e.g., Frontend, Backend, etc.)
-            $table->string('post_type')->default('snippet'); // snippet, tutorial, feedback, showcase, question, etc.
-            $table->string('status')->default('draft'); // status of the post (e.g., draft, private, published, archived)
+            $table->string('post_type'); // snippet, tutorial, feedback, showcase, question, etc.
+            $table->string('status'); // status of the post (e.g., draft, private, published, archived)
 
             // Counts
             $table->integer('favorite_count')->default(0);
