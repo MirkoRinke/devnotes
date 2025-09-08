4becd79ca41e786c179877d9fd0a0925d99892f5
MirkoRinke <mirkorinke@htp.com>
2025-09-09 01:51:15 +0200

    feat(PostQuerySetup): include 'is_important' in request select modification

--- app/Traits/PostQuerySetup.php
+++ app/Traits/PostQuerySetup.php
@@ -41,7 +41,7 @@ trait PostQuerySetup {
 
         $relationKeyFields = $this->getRelationKeyFields($request, ['user' => 'user_id']);
 
-        $this->modifyRequestSelect($request, [...['id'], ...$relationKeyFields], ['is_favorited', 'is_liked', 'is_read']);
+        $this->modifyRequestSelect($request, [...['id'], ...$relationKeyFields], ['is_favorited', 'is_liked', 'is_read', 'is_important']);
 
         $query = $this->loadUserRelation($request, $query, 'user_id');
 
