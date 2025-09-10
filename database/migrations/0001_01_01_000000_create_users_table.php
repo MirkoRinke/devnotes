2f45eb0fac7745bd2af4f483977aa42c762bcfb4
MirkoRinke <mirkorinke@htp.com>
2025-09-10 02:09:34 +0200

    fix(users migration): ensure 'name' field is unique in users table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
         Schema::create('users', function (Blueprint $table) {
             // Default
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('email')->unique()->nullable();
             $table->string('github_id')->unique()->nullable();
